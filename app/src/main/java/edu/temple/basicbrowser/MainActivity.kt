8733a4dc380105fd57b2f699d4a37f792e0599e5
son2005 <tranbaoson2005@gmail.com>
2022-10-27 15:17:09 -0400

    Complete assignment
    Vinh Le
    Hy Nguyen
    Son Tran
    Tony Zheng

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -2,10 +2,17 @@ package edu.temple.basicbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.io.StringReader
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,19 +20,48 @@ class MainActivity : AppCompatActivity() {
     lateinit var goButton: ImageButton
     lateinit var webView: WebView
 
+    lateinit var requestQueue: RequestQueue
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        requestQueue = Volley.newRequestQueue(this)
+
         urlEditText = findViewById(R.id.urlEditText)
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
+
+        // enable js
+        webView.settings.javaScriptEnabled = true
 
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 super.onPageFinished(view, url)
             }
+        }
+
+
+
+        goButton.setOnClickListener {
+
+            var url = urlEditText.text.toString()
+
+            if(!url.contains("https://") || !url.contains("http://")) {
+                // add https:// to ur string
+                url = "https://" + url
+            }
+
+
+            requestQueue.add(
+                StringRequest(Request.Method.GET, url, {
+                    webView.loadDataWithBaseURL("", it, "text/html", "utf-8", null)
+                }, {})
+            )
+
+
+
         }
 
     }
